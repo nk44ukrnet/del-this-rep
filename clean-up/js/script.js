9c9693af6ee0db7bb8edb7438bf7bb4548e8d76e
Віктор Ковтун <v.kovtun@ies.org.ua>
2020-10-26 18:01:55 +0200

    clean-up v2

--- clean-up/js/script.js
+++ clean-up/js/script.js
@@ -24,14 +24,17 @@ const movieDB = {
     ]
 };
 
+//sort movies
+
+
 
 //remove ads
 const promoImg = document.querySelectorAll('.promo__adv img');
 promoImg.forEach( (item) => {
    item.remove();
 });
-const promoAdv = document.querySelector('.promo__adv');
-promoAdv.remove();
+// const promoAdv = document.querySelector('.promo__adv');
+// promoAdv.remove();
 
 //change genre
 const genre = document.querySelector('.promo__genre');
@@ -45,4 +48,63 @@ promoBg.style.background = 'url(img/bg.jpg) center center/cover no-repeat';
 let listItemFilm = document.querySelectorAll('.promo__interactive-item');
 listItemFilm.forEach(function (item, num) {
    item.innerText =  `${num+1}. ${movieDB.movies[num]}`;
+});
+
+//extra1
+const inp = document.querySelector('.adding__input'),
+    submit = document.querySelector('button'),
+    promoUl = document.querySelector('.promo__interactive-list');
+
+submit.addEventListener('click', function (e) {
+    e.preventDefault();
+let currentVal = inp.value;
+
+if(currentVal.trim().innerText !== '') {
+    if(currentVal.length > 21) {
+        currentVal = currentVal.slice(0,21) + '...';
+    }
+    // promoUl.innerHTML += addToUlList(currentVal);
+    movieDB.movies.push(currentVal);
+    reRenderMovieDb();
+}
+    inp.value = '';
+});
+
+// function addToUlList(name) {
+//  return ` <li class="promo__interactive-item">${name}
+//                             <div class="delete"></div>
+//                         </li>`
+// }
+function reRenderMovieDb() {
+    const {movies} = movieDB;
+    promoUl.innerHTML = '';
+    movieDB.movies.sort();
+    for(let i = 0; i < movies.length; i++) {
+        promoUl.innerHTML += `
+    <li class="promo__interactive-item" data-number="${i}">${[i+1]}. ${movies[i]}
+                            <div class="delete"></div>
+                        </li>
+    `;
+    }
+}
+reRenderMovieDb();
+
+//del item
+promoUl.addEventListener('click', function (event) {
+    if(event.target.classList.contains('delete')) {
+        console.log('yes');
+        let filmNum = event.target.parentElement.dataset.number;
+        movieDB.movies.splice(filmNum, 1);
+        event.target.parentElement.remove();
+        reRenderMovieDb();
+    }
+    // const delItem = document.querySelectorAll('.delete');
+    // delItem.forEach(curEl => {
+    //     curEl.addEventListener('click', function (e) {
+    //         let filmNum = curEl.parentElement.dataset.number;
+    //         movieDB.movies.splice(filmNum, 1);
+    //         curEl.parentElement.remove();
+    //         reRenderMovieDb();
+    //     })
+    //});
 });
